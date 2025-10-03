efe9cf03303006f4cccf5423dacd913570643d2b
Vasile Draguta <dragutavasile@gmail.com>
2025-10-03 12:23:41 +0300

    added tiptap extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,9 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { TaskItem, TaskList } from '@tiptap/extension-list';
+import { TableKit } from '@tiptap/extension-table';
+import ImageResize from 'tiptap-extension-resize-image';
 
 export function Editor() {
   const editor = useEditor({
@@ -12,8 +15,35 @@ export function Editor() {
           'focus-outline-none print:border-0 print:bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
       },
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World</p>',
+    extensions: [
+      StarterKit,
+      TaskItem.configure({ nested: true }),
+      TaskList,
+      TableKit.configure({
+        table: { resizable: true },
+      }),
+      ImageResize,
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/800x400" />
+        <img src="https://placehold.co/800x400/6A00F5/white" />
+      `,
     immediatelyRender: false,
   });
   return (
